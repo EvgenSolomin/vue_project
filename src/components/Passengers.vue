f5858a0c0d88133982d70d809b38f1c7a784e91d
EvgenSolomin <solomin.hello@yandex.ru>
2024-02-04 18:41:20 +0300

    Create Passengers.vue

---
+++ src/components/Passengers.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+       <div class="main">
+            <span>Взрослый</span>
+            <Quantity/> 
+        </div>
+        <div class="main">
+            <span>Детский</span>
+            <Quantity/>
+        </div> 
+    </form>
+    
+    
+</template>
+<script>
+import Quantity from './Quantity.vue'
+
+export default{
+    components:{Quantity}
+}
+</script>
+<style scoped>
+*{
+    margin: 0;
+    padding: 5px;
+    
+}
+.main{
+    margin: auto;
+    display: flexbox;
+    justify-content: center;
+}
+</style>
