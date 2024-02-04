91071b26dace8ae6d73903d70562e0d608f77463
EvgenSolomin <solomin.hello@yandex.ru>
2024-02-04 18:41:16 +0300

    Create Dates.vue

---
+++ src/components/Dates.vue
@@ -0,0 +1,26 @@
+<template>
+    <form>
+        <input type="date">
+        <input v-if="check" type="date" >
+        <form>
+           <input v-model="check" type="checkbox">Туда-обратно 
+        </form>
+    </form>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            check: false
+        }
+    }
+}
+</script>
+
+<style>
+*{
+    margin: 5px;
+}
+</style>
+
