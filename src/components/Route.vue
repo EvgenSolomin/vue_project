027cc5c4e041277d873e8c1d7c7404ae17b39eff
EvgenSolomin <solomin.hello@yandex.ru>
2024-02-04 18:41:26 +0300

    Create Route.vue

---
+++ src/components/Route.vue
@@ -0,0 +1,33 @@
+<template>
+    <form @submit.prevent>
+        <input v-model="pointA" type="text" placeholder="Откуда">
+        <button @click="reverse" >=</button>
+        <input v-model="pointB" type="text" placeholder="Куда">
+    </form>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            pointA:'',
+            pointB:'', 
+            rev:''
+        }
+    },
+    methods:{
+        reverse(){
+            this.rev = this.pointA
+            this.pointA = this.pointB
+            this.pointB = this.rev
+        }
+    }
+}
+</script>
+
+<style scoped>
+*{
+margin: 5px;
+}
+</style>
+
