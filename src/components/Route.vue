28834e8690e75d5ae38d0d3f86a2c8d0fe6adb12
EvgenSolomin <solomin.hello@yandex.ru>
2024-02-12 20:00:45 +0300

    Update Route.vue

--- src/components/Route.vue
+++ src/components/Route.vue
@@ -1,33 +1,43 @@
 <template>
     <form @submit.prevent>
-        <input v-model="pointA" type="text" placeholder="Откуда">
-        <button @click="reverse" >=</button>
-        <input v-model="pointB" type="text" placeholder="Куда">
+        <InputText v-model="pointA" type="text" placeholder="Откуда"/>
+        <Button @click="reverse" icon="pi pi-arrows-h" 
+        :pt="{ 
+            root: { class: 'bg-indigo-600 border-indigo-600' } 
+        }"></Button>
+        <InputText v-model="pointB" type="text" placeholder="Куда"/>
     </form>
 </template>
 
 <script>
+
 export default{
-    data(){
-        return{
-            pointA:'',
-            pointB:'', 
-            rev:''
-        }
+    data() {
+        return {
+            pointA: '',
+            pointB: '',
+            rev: ''
+        };
     },
-    methods:{
-        reverse(){
-            this.rev = this.pointA
-            this.pointA = this.pointB
-            this.pointB = this.rev
+    methods: {
+        reverse() {
+            this.rev = this.pointA;
+            this.pointA = this.pointB;
+            this.pointB = this.rev;
         }
     }
 }
 </script>
 
 <style scoped>
+button{
+    width: 30px;
+    height: 30px;
+    margin: 5px;
+}
 *{
-margin: 5px;
+    display: flex;
+    height: 30px;
 }
 </style>
 
