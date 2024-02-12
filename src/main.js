74a9a75b01868b3ad844a0d99864d766a5b4e170
EvgenSolomin <solomin.hello@yandex.ru>
2024-02-12 20:00:49 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import './assets/main.css'
-
+import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+import Calendar from 'primevue/calendar';
+import InputText from 'primevue/inputtext';
+import Checkbox from 'primevue/checkbox';
 
-createApp(App).mount('#app')
+
+
+
+const app = createApp(App);
+app.use(PrimeVue)
+app.component('Button', Button)
+app.component('Calendar', Calendar)
+app.component('InputText', InputText)
+app.component('Checkbox', Checkbox)
+app.mount('#app')
