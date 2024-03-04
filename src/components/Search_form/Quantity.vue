b77fe16f62d70daf5bafa6313efa36b75eac53f1
EvgenSolomin <solomin.hello@yandex.ru>
2024-03-04 21:35:07 +0300

    Create Quantity.vue

---
+++ src/components/Search_form/Quantity.vue
@@ -0,0 +1,41 @@
+<template>
+    <form @submit.prevent class="quan">
+        <Button @click="minus">-</Button>
+        {{ passen }}
+        <Button @click="plus">+</Button>
+    </form>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            passen: 0
+        }
+    },
+    methods:{
+        plus(){
+            if(this.passen >= 0){
+                this.passen ++
+            }
+        }, 
+        minus(){
+            if(this.passen > 0){
+                this.passen --
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+button{
+    width: 20px;
+    height: 20px;
+    margin: 5px;
+}
+form{
+    display: flex;
+    align-items: center;
+}
+</style>
