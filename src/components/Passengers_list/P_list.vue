98e8457d55ae739b675d583100eb502d7758f186
EvgenSolomin <solomin.hello@yandex.ru>
2024-03-04 21:34:52 +0300

    Create P_list.vue

---
+++ src/components/Passengers_list/P_list.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="form">
+        <P_form/>
+    </div>
+</template>
+
+<script>
+import P_form from './P_form.vue'
+export default{
+    components:{ P_form }
+}
+
+</script>
+
+<style scoped>
+.form{
+    display: flex;
+    justify-content: left;
+    background-color:rgb(228, 241, 255);
+    height: 300px;
+}
+</style>
