da03cd9961e8d2fe9ce019c5236c24a60c82dddc
EvgenSolomin <solomin.hello@yandex.ru>
2024-03-05 19:54:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import 'primeicons/primeicons.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-blue/theme.css'
+
 import Button from 'primevue/button';
 import Calendar from 'primevue/calendar';
 import InputText from 'primevue/inputtext';
@@ -10,6 +12,8 @@ import Checkbox from 'primevue/checkbox';
 import InputMask from 'primevue/inputmask';
 import FloatLabel from 'primevue/floatlabel';
 import Dropdown from 'primevue/dropdown';
+import Card from 'primevue/card';
+import Panel from 'primevue/panel';
 
 const app = createApp(App);
 app.use(PrimeVue)
@@ -20,4 +24,6 @@ app.component('Checkbox', Checkbox)
 app.component('InputMask', InputMask)
 app.component('FloatLabel', FloatLabel)
 app.component('Dropdown', Dropdown)
+app.component('Card', Card)
+app.component('Panel', Panel)
 app.mount('#app')
