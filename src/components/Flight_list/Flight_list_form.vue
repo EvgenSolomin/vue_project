5dcab0c9bf9a8a63ecff25a8f5432fc2bcd389e0
vfz <stavfz@gmail.com>
2024-03-07 11:46:41 +0300

    z-index button pay

--- src/components/Flight_list/Flight_list_form.vue
+++ src/components/Flight_list/Flight_list_form.vue
@@ -2,7 +2,7 @@
     <div class="overflow-hidden">
         <div class="overflow-auto border-round border-1 border-primary-500" style="height: 470px">
             <div v-for="i in 10" :key="i">
-                <div class="sticky top-0 font-bold bg-yellow-300 p-2 pl-4">
+                <div class="sticky top-0 font-bold bg-yellow-300 p-2 pl-4 z-1">
                     Рейс № {{ i }}
                 </div>
                 <p class="surface-overlay p-2 m-0">
@@ -10,7 +10,7 @@
                 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
                 </p>
                 <div class="flex justify-content-end flex-wrap">
-                    <Button label="Купить" class="m-2" />
+                    <Button label="Купить" class="m-2 z-0" />
                 </div>
             </div>
         </div>
