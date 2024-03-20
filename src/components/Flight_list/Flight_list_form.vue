e71aee07c37276682101a4d8059f06d9d3446cf9
EvgenSolomin <solomin.hello@yandex.ru>
2024-03-20 17:10:12 +0300

    20.03

--- src/components/Flight_list/Flight_list_form.vue
+++ src/components/Flight_list/Flight_list_form.vue
@@ -2,12 +2,11 @@
     <div class="overflow-hidden">
         <div class="overflow-auto border-round border-1 border-primary-500" style="height: 470px">
             <div v-for="i in 10" :key="i">
-                <div class="sticky top-0 font-bold bg-yellow-300 p-2 pl-4 z-1">
+                <div class="sticky top-0 font-bold bg-blue-500 p-2 pl-4 z-1">
                     Рейс № {{ i }}
                 </div>
                 <p class="surface-overlay p-2 m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.
+                    Пусто
                 </p>
                 <div class="flex justify-content-end flex-wrap">
                     <Button label="Купить" class="m-2 z-0" />
--- src/components/Passengers_list/P_form.vue
+++ src/components/Passengers_list/P_form.vue
@@ -8,15 +8,15 @@
                 Список пассажиров пуст
             </div>
             <div class="flex flex-column w-2 p-1">
-                <InputText v-model="passeger.name" placeholder="Фамилия" />
-                <InputText v-model="passeger.surname" placeholder="Имя"/>
-                <InputText v-model="passeger.patronymic" placeholder="Отчество"/>
-                <Calendar v-model="passeger.bd" showIcon  placeholder="Дата рождения"/>
-                <Dropdown v-model="passeger.sex" placeholder="Пол" :options="sexarr" />
-                <InputText v-model="passeger.nationality" placeholder="Гражданство"  />
-                <Dropdown v-model="passeger.doc" placeholder="Документ" :options="docarr"/>
-                <InputMask v-model="passeger.passport" v-if="passeger.doc != ''" placeholder="Серия/Номер паспорта" mask="9999/999-999"/>
-                <Button label="Добавить" @click="creatPassager"/>
+                <InputText class="m-1" v-model="passeger.name" placeholder="Фамилия" />
+                <InputText class="m-1"  v-model="passeger.surname" placeholder="Имя"/>
+                <InputText class="m-1" v-model="passeger.patronymic" placeholder="Отчество"/>
+                <Calendar class="m-1" v-model="passeger.bd" showIcon  placeholder="Дата рождения"/>
+                <Dropdown class="m-1" v-model="passeger.sex" placeholder="Пол" :options="sexarr" />
+                <InputText class="m-1" v-model="passeger.nationality" placeholder="Гражданство"  />
+                <Dropdown class="m-1" v-model="passeger.doc" placeholder="Документ" :options="docarr"/>
+                <InputMask class="m-1" v-model="passeger.passport" v-if="passeger.doc != ''" placeholder="Серия/Номер паспорта" mask="9999/999-999"/>
+                <Button class="m-1" label="Добавить" @click="creatPassager"/>
             </div>            
         </template>
     </Card>
