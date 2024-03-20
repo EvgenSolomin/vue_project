30c46d3d8edf11ee5800a316902c6e0c1a29f5af
vfz <stavfz@gmail.com>
2024-03-20 17:21:37 +0300

    delete form in SearchForm

--- src/components/Search_form/Passengers.vue
+++ src/components/Search_form/Passengers.vue
@@ -1,17 +1,14 @@
 <template>
-    <form>
-        <div class=" flex flex-row justify-content-center" >
-            <div class="flex flex-column justify-content-center align-items-center m-1">
-                <span>Взрослый</span>
-                <Quantity /> 
-            </div>
-            <div class="flex flex-column justify-content-center align-items-center m-1">
-                <span>Детский</span>
-                <Quantity/>
-            </div>
+    <div class=" flex flex-row justify-content-center" >
+        <div class="flex flex-column justify-content-center align-items-center m-1">
+            <span>Взрослый</span>
+            <Quantity /> 
         </div>
-        
-    </form>
+        <div class="flex flex-column justify-content-center align-items-center m-1">
+            <span>Детский</span>
+            <Quantity/>
+        </div>
+    </div>
 </template>
 <script>
 import Quantity from './Quantity.vue'
--- src/components/Search_form/Quantity.vue
+++ src/components/Search_form/Quantity.vue
@@ -1,12 +1,9 @@
 <template>
-    <form @submit.prevent>
-        <div class="flex justify-content-center align-items-center m-1">
-            <Button class="m-1 " @click="minus">-</Button>
-            {{ passen }}
-            <Button class="m-1" @click="plus">+</Button>
-        </div>
-        
-    </form>
+    <div class="flex justify-content-center align-items-center m-1">
+        <Button class="m-1 " @click="minus">-</Button>
+        {{ passen }}
+        <Button class="m-1" @click="plus">+</Button>
+    </div>
 </template>
 
 <script>
