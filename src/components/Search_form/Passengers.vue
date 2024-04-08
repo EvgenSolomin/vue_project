3c6903fbf323c82c39710e20bf36e951e7273a7b
EvgenSolomin <solomin.hello@yandex.ru>
2024-04-08 21:34:54 +0300

    defaul Quantity on composition

--- src/components/Search_form/Passengers.vue
+++ src/components/Search_form/Passengers.vue
@@ -2,24 +2,19 @@
     <div class=" flex flex-row justify-content-center" >
         <div class="flex flex-row justify-content-center align-items-center m-1">
             <span>Взрослый</span>
-            <Quantity :passengers="passengers" /> 
+            <Quantity :passengers_type="passengers_type.adult"/>
         </div>
         <div class="flex flex-row justify-content-center align-items-center m-1">
             <span>Детский</span>
-            <Quantity :passengers="child"/>
+            <Quantity :passengers_type="passengers_type.child"/>
         </div>
     </div>
 </template>
-<script>
-import Quantity from './Quantity.vue'
-
-export default{
-    components:{Quantity},
-    data(){
-        return{
-            passengers: 1,
-            child: 0
-        }
-    }
-}
+<script setup>
+import {ref, provide, toRefs} from 'vue'
+import Quantity from './Quantity.vue';
+const passengers_type = {
+    adult: "adult",
+    child: "child"
+} 
 </script>
--- src/components/Search_form/Quantity.vue
+++ src/components/Search_form/Quantity.vue
@@ -1,36 +1,33 @@
 <template>
     <div class="flex justify-content-center align-items-center m-1">
-        <Button :disabled="p == passengers" label="-" class="flex justify-content-center m-1" style="height: 30px; width: 30px" @click="minus" />
-        {{ p }}
-        <Button label="+" class="flex justify-content-center m-1" style="height: 30px; width: 30px" @click="plus" />
+        <Button :disabled="passengers == default_passagers" label="-" class="flex justify-content-center m-1" style="height: 30px; width: 30px" @click="raise(false)" />
+            {{ passengers }}
+        <Button label="+" class="flex justify-content-center m-1" style="height: 30px; width: 30px" @click="raise(true)" />
     </div>
 </template>
 
-<script>
-export default{
-    props:{
-        passengers: {
-            type: Number,
-            required: true
-        }
-    },
-    data(){
-        return{
-            p: this.passengers,
-            bull: true
-        }
-    },
-    methods:{
-        plus(){
-            if(this.p >= 0){
-                return this.p ++
-            };
-        }, 
-        minus(){
-            if(this.p > this.passengers){
-                return this.p --
-            };
-        }
-    } 
+<script setup>
+import {ref} from 'vue'
+
+const p_type = defineProps({
+    passengers_type: String
+})
+let default_passagers = null;
+
+if(p_type.passengers_type == "adult")
+    default_passagers = 1;
+else
+    default_passagers = 0;
+
+const passengers = ref(default_passagers)
+
+function raise(increase){
+    if(increase)
+        this.passengers++
+    else if (!increase && this.passengers > this.default_passagers)
+        this.passengers--
 }
+
+
+
 </script>
